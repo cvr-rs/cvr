25e1eb13d077d2dac038b89bc1b5bfa9c223ee5e
LeonineKing1199 <christian.mazakas@gmail.com>
2021-04-20 22:39:52 -0700

    Add `rgba::IterMut`

--- src/rgba.rs
+++ src/rgba.rs
@@ -3,6 +3,8 @@
 
 extern crate minivec;
 
+use crate::Numeric;
+
 /// `Image` represents any `RGBA` image. Internally, it stores each channel as an independent
 /// allocation which enables such things as constant-time channel swapping along with making the
 /// data cheaper to copy to a GPU which expects `CHW` ordering vs the packed format `HWC`.
@@ -10,7 +12,7 @@ extern crate minivec;
 #[derive(Default)]
 pub struct Image<T>
 where
-  T: crate::Numeric,
+  T: Numeric,
 {
   pub(super) r: minivec::MiniVec<T>,
   pub(super) g: minivec::MiniVec<T>,
@@ -22,7 +24,7 @@ where
 
 impl<T> Image<T>
 where
-  T: crate::Numeric,
+  T: Numeric,
 {
   /// `r` returns an immutable reference to the image's red channel as a `&[T]`.
   ///
@@ -87,7 +89,7 @@ where
 ///
 pub struct Iter<'a, N>
 where
-  N: crate::Numeric,
+  N: Numeric,
 {
   r: std::slice::Iter<'a, N>,
   g: std::slice::Iter<'a, N>,
@@ -109,7 +111,7 @@ where
 ///
 impl<'a, N> Iter<'a, N>
 where
-  N: crate::Numeric,
+  N: Numeric,
 {
   /// `new` returns an [`Iter`] that traverses the provided slices.
   ///
@@ -128,13 +130,87 @@ where
 
 impl<'a, N> std::iter::Iterator for Iter<'a, N>
 where
-  N: crate::Numeric,
+  N: Numeric,
 {
   type Item = [N; 4];
 
   fn next(&mut self) -> Option<Self::Item> {
     match (self.r.next(), self.g.next(), self.b.next(), self.a.next()) {
       (Some(r), Some(g), Some(b), Some(a)) => Some([*r, *g, *b, *a]),
+      _ => None,
+    }
+  }
+}
+
+/// `IterMut` enables the simultaneous traversal of 4 separate channels of image data. It works
+/// with any type that can be converted to a `&mut [Numeric]`. Image data is returned pixel-by-pixel
+/// in a `[&'a mut T; 4]` format with `(R, G, B, A)` ordering.
+///
+pub struct IterMut<'a, T>
+where
+  T: Numeric,
+{
+  r: std::slice::IterMut<'a, T>,
+  g: std::slice::IterMut<'a, T>,
+  b: std::slice::IterMut<'a, T>,
+  a: std::slice::IterMut<'a, T>,
+}
+
+impl<'a, T> IterMut<'a, T>
+where
+  T: Numeric,
+{
+  /// `new` constructs a new `IterMut` over the backing `&'a mut [T]` of each `&'a mut U` supplied by the user.
+  ///
+  /// # Example
+  /// ```
+  /// let mut r = vec![0_u8; 2];
+  /// let mut g = vec![0_u8; 2];
+  /// let mut b = vec![0_u8; 2];
+  /// let mut a = vec![0_u8; 2];
+  ///
+  /// let rgb_iter = cvr::rgba::IterMut::new(&mut r, &mut g, &mut b, &mut a);
+  ///
+  /// let pixels = [1, 2, 3, 4, 5, 6]; // assume only 2 pixels in the buffer (packed representation)
+  /// pixels
+  ///   .chunks_exact(3)
+  ///   .zip(rgb_iter)
+  ///   .for_each(|(chunk, [r, g, b, a])| {
+  ///     *r = chunk[0];
+  ///     *g = chunk[1];
+  ///     *b = chunk[2];
+  ///     *a = 128;
+  ///   });
+  ///
+  /// assert_eq!(r, [1, 4]);
+  /// assert_eq!(g, [2, 5]);
+  /// assert_eq!(b, [3, 6]);
+  /// assert_eq!(a, [128, 128]);
+  ///
+  /// ```
+  ///
+  pub fn new<U>(r: &'a mut U, g: &'a mut U, b: &'a mut U, a: &'a mut U) -> Self
+  where
+    U: std::convert::AsMut<[T]>,
+  {
+    Self {
+      r: r.as_mut().iter_mut(),
+      g: g.as_mut().iter_mut(),
+      b: b.as_mut().iter_mut(),
+      a: a.as_mut().iter_mut(),
+    }
+  }
+}
+
+impl<'a, T> std::iter::Iterator for IterMut<'a, T>
+where
+  T: Numeric,
+{
+  type Item = [&'a mut T; 4];
+
+  fn next(&mut self) -> Option<Self::Item> {
+    match (self.r.next(), self.g.next(), self.b.next(), self.a.next()) {
+      (Some(r), Some(g), Some(b), Some(a)) => Some([r, g, b, a]),
       _ => None,
     }
   }

6f0388900099a2416a036e583c3db15970b26dd2
LeonineKing1199 <christian.mazakas@gmail.com>
2021-05-26 14:32:58 -0700

    Clean previously generated test images

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 cargo clippy \
   && cargo build \
+  && (rm tests/images/output/*.png || echo "no test-generated images to remove") \
   && CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_RUNNER="valgrind" \
      cargo test --release $@ \
   && cargo doc \
